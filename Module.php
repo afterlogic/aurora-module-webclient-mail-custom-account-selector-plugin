99774f71f9a88cacb592b6caaa75abd023c67808
Vasil Sokolov <vasil@afterlogic.com>
2023-01-27 12:45:57 +0200

    Added CS Fixer check to pre-commit

--- Module.php
+++ Module.php
@@ -16,70 +16,64 @@ namespace Aurora\Modules\MailCustomAccountSelectorWebclientPlugin;
  */
 class Module extends \Aurora\System\Module\AbstractModule
 {
-	public function init() 
-	{
-	}
+    public function init()
+    {
+    }
 
-	/**
-	 * Obtains list of module settings.
-	 * @return array
-	 */
-	public function GetSettings()
-	{
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
+    /**
+     * Obtains list of module settings.
+     * @return array
+     */
+    public function GetSettings()
+    {
+        \Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
 
-		return [
-			'NumberOfAccountsToDisplay' => $this->getConfig('NumberOfAccountsToDisplay', 3)
-		];
-	}
+        return [
+            'NumberOfAccountsToDisplay' => $this->getConfig('NumberOfAccountsToDisplay', 3)
+        ];
+    }
 
-	/**
-	 * Obtains relevant folders information for several accounts
-	 * @return array
-	 */
-	public function GetAccountsRelevantFoldersInformation($AccountsData)
-	{
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
+    /**
+     * Obtains relevant folders information for several accounts
+     * @return array
+     */
+    public function GetAccountsRelevantFoldersInformation($AccountsData)
+    {
+        \Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
 
-		$aResultAccountsData = [];
-		foreach ($AccountsData as $aAccountData)
-		{
-			$iAccountId = $aAccountData['AccountID'];
-			$oAccount = \Aurora\Modules\Mail\Module::Decorator()->GetAccount($iAccountId);
-			if ($oAccount instanceof \Aurora\Modules\Mail\Models\MailAccount)
-			{
-				\Aurora\Modules\Mail\Module::checkAccess($oAccount);
-				$aCounts = \Aurora\Modules\Mail\Module::Decorator()->GetRelevantFoldersInformation($iAccountId, $aAccountData['Folders'], $aAccountData['UseListStatusIfPossible']);
-				$aCounts['AccountId'] = $iAccountId;
-				$aResultAccountsData[] = $aCounts;
-			}
-		}
-		return ['Accounts' => $aResultAccountsData];
-	}
-	
-	public function SaveAccountsData($AccountsData)
-	{
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
+        $aResultAccountsData = [];
+        foreach ($AccountsData as $aAccountData) {
+            $iAccountId = $aAccountData['AccountID'];
+            $oAccount = \Aurora\Modules\Mail\Module::Decorator()->GetAccount($iAccountId);
+            if ($oAccount instanceof \Aurora\Modules\Mail\Models\MailAccount) {
+                \Aurora\Modules\Mail\Module::checkAccess($oAccount);
+                $aCounts = \Aurora\Modules\Mail\Module::Decorator()->GetRelevantFoldersInformation($iAccountId, $aAccountData['Folders'], $aAccountData['UseListStatusIfPossible']);
+                $aCounts['AccountId'] = $iAccountId;
+                $aResultAccountsData[] = $aCounts;
+            }
+        }
+        return ['Accounts' => $aResultAccountsData];
+    }
 
-		foreach ($AccountsData as $aAccountData)
-		{
-			$iAccountId = $aAccountData['AccountID'];
-			$oAccount = \Aurora\Modules\Mail\Module::Decorator()->GetAccount($iAccountId);
-			if ($oAccount instanceof \Aurora\Modules\Mail\Models\MailAccount)
-			{
-				\Aurora\Modules\Mail\Module::checkAccess($oAccount);
-				if (isset($aAccountData['MailboxName']))
-				{
-					$oAccount->{self::GetName() . '::MailboxName'} = $aAccountData['MailboxName'];
-				}
-				if (isset($aAccountData['MailboxPosition']))
-				{
-					$oAccount->{self::GetName() . '::MailboxPosition'} = $aAccountData['MailboxPosition'];
-				}
-				$oAccount->save();
-			}
-		}
+    public function SaveAccountsData($AccountsData)
+    {
+        \Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
 
-		return true;
-	}
+        foreach ($AccountsData as $aAccountData) {
+            $iAccountId = $aAccountData['AccountID'];
+            $oAccount = \Aurora\Modules\Mail\Module::Decorator()->GetAccount($iAccountId);
+            if ($oAccount instanceof \Aurora\Modules\Mail\Models\MailAccount) {
+                \Aurora\Modules\Mail\Module::checkAccess($oAccount);
+                if (isset($aAccountData['MailboxName'])) {
+                    $oAccount->{self::GetName() . '::MailboxName'} = $aAccountData['MailboxName'];
+                }
+                if (isset($aAccountData['MailboxPosition'])) {
+                    $oAccount->{self::GetName() . '::MailboxPosition'} = $aAccountData['MailboxPosition'];
+                }
+                $oAccount->save();
+            }
+        }
+
+        return true;
+    }
 }
